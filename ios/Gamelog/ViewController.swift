e4fd4046e081a448c141f7e757b912b6f1330962
Francis Bourque <francis@studyo.co>
2020-04-26 11:35:16 -0400

    Create ios project

---
+++ ios/Gamelog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gamelog
+//
+//  Created by Francis Bourque on 2020-04-26.
+//  Copyright © 2020 Francis Bourque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios/GamelogTests/GamelogTests.swift
@@ -0,0 +1,34 @@
+//
+//  GamelogTests.swift
+//  GamelogTests
+//
+//  Created by Francis Bourque on 2020-04-26.
+//  Copyright © 2020 Francis Bourque. All rights reserved.
+//
+
+import XCTest
+@testable import Gamelog
+
+class GamelogTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
